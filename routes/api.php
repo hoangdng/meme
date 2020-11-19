38e04e643820cf2adfae2bf7e3e4df91dcbf809a
Hoang <nguyenhoangdng43@gmail.com>
2020-11-19 21:03:10 +0700

    Protect routes for members and admin only

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -11,14 +9,18 @@ use Illuminate\Http\Request;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-
+ */
 
 Route::post('accounts/post', 'AccountController@store');
 
 Route::post('login', 'AuthController@authenticate');
 
-Route::get('users', 'UserController@index');
-Route::get('users/{id}', 'UserController@show');
-Route::patch('users/{id}', 'UserController@update');
-Route::delete('users/{id}', 'UserController@delete');
+//Routes for admin
+Route::group(['middleware' => ['api', 'jwt.verify']], function () {
+    Route::get('users/{id}', 'UserController@show');
+    Route::group(['middleware' => ['admin']], function () {
+        Route::get('users', 'UserController@index');
+        Route::patch('users/{id}', 'UserController@update');
+        Route::delete('users/{id}', 'UserController@delete');
+    });
+});
