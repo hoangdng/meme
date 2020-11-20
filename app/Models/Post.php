de8a4c90a9965ee64cf0d13606f25b4631b0cc46
Hoang <nguyenhoangdng43@gmail.com>
2020-11-20 16:33:05 +0700

    Create model for Post entity

---
+++ app/Models/Post.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'image_location',
+        'posted_date',
+        'username',
+        'status',
+        'vote_up',
+        'vote_down',
+    ];
+}
