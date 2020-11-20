f2878bc714ef4d9d56a98aa181c93178409d7303
Hoang <nguyenhoangdng43@gmail.com>
2020-11-20 16:33:34 +0700

    Create migration for Post model

---
+++ database/migrations/2020_11_20_085525_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('image_location');
+            $table->date('posted_date');
+            $table->integer('vote_up');
+            $table->integer('vote_down');
+            $table->string('status');
+            $table->string('username');
+            $table->foreign('username')->references('username')->on('users');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
