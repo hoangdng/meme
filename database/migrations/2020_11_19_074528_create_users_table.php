fe61e54d91a1194d9c630cb2e06235738332fa45
Hoang <nguyenhoangdng43@gmail.com>
2020-11-20 16:32:40 +0700

    Set field username to be unique in table users

--- database/migrations/2020_11_19_074528_create_users_table.php
+++ database/migrations/2020_11_19_074528_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('username');
+            $table->string('username')->unique();
             $table->foreign('username')->references('username')->on('accounts');
             $table->string('avatar')->nullable();
             $table->string('status');
