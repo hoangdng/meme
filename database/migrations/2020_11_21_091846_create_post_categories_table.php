1270ca77f26c7311f7b26fd02d0d9a0009e424aa
Hoang <nguyenhoangdng43@gmail.com>
2020-11-21 16:39:14 +0700

    Create migration for intermediate table post_category

---
+++ database/migrations/2020_11_21_091846_create_post_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_categories', function (Blueprint $table) {
+            $table->primary(['post_id','category_id']);
+            $table->integer('post_id');
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->integer('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_categories');
+    }
+}
