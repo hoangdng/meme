d9b2dfc1670b615c57faca1fc459999f44005e1f
Hoang <nguyenhoangdng43@gmail.com>
2020-11-22 17:32:02 +0700

    Finish CRUD report feature

---
+++ database/migrations/2020_11_22_101642_create_reports_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('post_id');
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->string('username');
+            $table->foreign('username')->references('username')->on('users');
+            $table->string('content');
+            $table->string('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
